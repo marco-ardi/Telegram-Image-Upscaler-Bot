882821fb7978121754262bf0e9a58d8baa029e43
marco-ardi <marco.ardizzone99@gmail.com>
2022-02-14 16:38:25 +0100

    tiny fixes

--- bot.py
+++ bot.py
@@ -1,21 +1,14 @@
 """
-Simple Bot to reply to Telegram messages.
-
-First, a few handler functions are defined. Then, those functions are passed to
-the Dispatcher and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
+Simple Bot to upscale images (x2, x3 or x4 upscale is currently available)
 
 Usage:
-Basic Echobot example, repeats messages.
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
+Click on upscale, then choose your upscale level and send your image, the bot
+will return the upscaled bot using image_enhancer.
 """
-import time
 import os
 import logging
 from telegram import BotCommand, KeyboardButton, ReplyKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Dispatcher
-
 from image_enhancer import imageEnhancer
 # log info
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -23,13 +16,16 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 TOKEN = "5148449384:AAGSgPkYDmGGt-Hh1pvNv-SUkWK_YuxgfgQ"
 
-# command handlers
-# def start(update, context):
-#    update.message.reply_text("Hi!")
+
+def start(update, context):
+    buttons = [[KeyboardButton("Upscale")], [KeyboardButton("Help")]]
+    update.message.reply_text(
+        "Welcome", reply_markup=ReplyKeyboardMarkup(buttons))
 
 
 def help(update, context):
-    update.message.reply_text("Help!")
+    update.message.reply_text(
+        "Welcome to Image Upscaler Bot! \n Click on Upscale, then choose your upscale factor and send your image")
 
 
 def recognize(update, context):
@@ -40,7 +36,6 @@ def image_handler(update, context):
     file = update.message.photo[-1].file_id
     obj = context.bot.get_file(file)
     obj.download("./" + str(update.message.from_user.id) + "photo.png")
-    #update.message.reply_text("What kind of upscale do you want? \n x2 \t x3 \t x4")
 
 
 def text_handler(update, context):
@@ -52,21 +47,22 @@ def text_handler(update, context):
             KeyboardButton("x4")]]
         update.message.reply_text(
             "Please choose your upscale level", reply_markup=ReplyKeyboardMarkup(buttons))
-        #style = update.message.text[1]
         update.message.reply_text("Please send your image to upscale")
         path = "./" + str(update.message.from_user.id) + ".png"
     if "x2" in update.message.text or "x3" in update.message.text or "x4" in update.message.text:
         style = update.message.text
+        start_buttons = [[KeyboardButton("Upscale")], [KeyboardButton("Help")]]
+        update.message.reply_text("Please wait until your image is upscaled!",
+                                  reply_markup=ReplyKeyboardMarkup(start_buttons))
 
 
-def singleMethod(update, context):
+def image_handler(update, context):
     global enhancer
     global path
     global style
 
     file = update.message.photo[-1].file_id
     obj = context.bot.get_file(file)
-    #path = "./" + str(update.message.from_user.id) + ".png"
     obj.download(path)
     print(f"style={style} \t path={path}")
     if style == "x2":
@@ -75,9 +71,10 @@ def singleMethod(update, context):
         enhancer.Enhance("x3", path)
     elif style == "x4":
         enhancer.Enhance("x4", path)
-    #update.message.reply_text("What kind of upscale do you want? \n x2 \t x3 \t x4")
     if os.path.isfile(path[:len(path)-4] + "out.png"):
         update.message.reply_photo(photo=open(
+            path[:len(path)-4] + "out.png", "rb"))
+        update.message.reply_document(document=open(
             path[:len(path)-4] + "out.png", "rb"))
         os.remove(path)
         os.remove(path[:len(path)-4] + "out.png")
@@ -89,11 +86,9 @@ def error(update, context):
 
 def add_commands(up: Updater):
     commands = [
-        BotCommand("start", "Introduction to Bot"),
+        BotCommand("start", "Upscale your image"),
         BotCommand("help", "Gives help"),
-        BotCommand("singleMethod", "Upscale your image")
     ]
-
     up.bot.set_my_commands(commands=commands)
 
 
@@ -101,37 +96,19 @@ def add_handlers(dp: Dispatcher):
     # commands
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
-    #dp.add_handler(CommandHandler("singleMethod", singleMethod))
     # messages
-    dp.add_handler(MessageHandler(Filters.photo, singleMethod))
+    dp.add_handler(MessageHandler(Filters.text, text_handler))
+    dp.add_handler(MessageHandler(Filters.photo, image_handler))
     # errors
     dp.add_error_handler(error)
-
-
-def start(update, context):
-    buttons = [[KeyboardButton("Upscale")], [KeyboardButton("Help")]]
-    update.message.reply_text(
-        "Welcome", reply_markup=ReplyKeyboardMarkup(buttons))
 
 
 def main():
     # updater for bot
     updater = Updater(TOKEN, use_context=True)
-
-    #enhancer = imageEnhancer()
-    # dispatcher
     dp = updater.dispatcher
-    # add_commands(updater)
-    # add_handlers(updater.dispatcher)
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
-    dp.add_handler(CommandHandler("recognize", recognize))
-    dp.add_handler(MessageHandler(Filters.text, text_handler))
-    # need to pass enhancer
-    dp.add_handler(MessageHandler(Filters.photo, singleMethod))
-    # dp.add_handler(MessageHandler(Filters.text & Filters.photo, singleMethod))#need to pass enhancer
-    # log all errors
-    # dp.add_error_handler(error)
+    add_commands(updater)
+    add_handlers(updater.dispatcher)
     updater.start_polling()
     updater.idle()
 
--- image_enhancer.py
+++ image_enhancer.py
@@ -1,40 +1,43 @@
+import torch
 import cv2
 from cv2 import dnn_superres
-#creating an sr object
+# creating an sr object
+
+
 class imageEnhancer:
     sr2 = dnn_superres.DnnSuperResImpl_create()
     sr3 = dnn_superres.DnnSuperResImpl_create()
     sr4 = dnn_superres.DnnSuperResImpl_create()
 
     def __init__(self):
-        #sr2 = dnn_superres.DnnSuperResImpl_create()
+        # Loading models
         self.sr2.readModel("./models/EDSR_x2.pb")
-        self.sr2.setModel("edsr", 2)
         self.sr3.readModel("./models/EDSR_x3.pb")
-        self.sr3.setModel("edsr", 3)
         self.sr4.readModel("./models/EDSR_x4.pb")
+
+        # sets GPU instead of CPU if CUDA is available
+        if torch.cuda.is_available():
+            print("Setting GPU instead of CPU")
+            self.sr2.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+            self.sr2.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+            self.sr3.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+            self.sr3.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+            self.sr4.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+            self.sr4.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+        # setting models
+        self.sr2.setModel("edsr", 2)
+        self.sr3.setModel("edsr", 3)
         self.sr4.setModel("edsr", 4)
-    
-    def Enhance(self, sr_type : str, path : str) : 
+
+    def Enhance(self, sr_type: str, path: str):
+        '''
+        Upscale the input image
+        '''
         img = cv2.imread(path)
         if sr_type == "x2":
             result = self.sr2.upsample(img)
         elif sr_type == "x3":
             result = self.sr3.upsample(img)
         elif sr_type == "x4":
-            result = self.sr2.upsample(img)
+            result = self.sr4.upsample(img)
         cv2.imwrite(path[:len(path)-4] + "out.png", result)
-    #sr = dnn_superres.DnnSuperResImpl_create()
-
-    #image = cv2.imread("./input.png")
-    #load the desired model
-    #path = "./models/EDSR_x4.pb"
-    #sr.readModel(path)
-    #set the desired model and scale
-    #sr.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
-    #sr.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
-    
-    #sr.setModel("edsr", 4)
-    #upscale and save
-    #result = sr.upsample(image)
-    #cv2.imwrite("./output.png", result)
